246a40620d441341c9d4c34f50cfe9424a23a2fb
John Heberth <johnpereira.df@gmail.com>
2023-03-27 18:52:01 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+
+        ENTRADA DE DADOS EM JAVA
+
+        int x;
+        String s1, s2, s3;
+
+        Scanner sc = new Scanner (System.in);
+
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Dados Digitados");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);*/
+
+// -------------------------------------------------
+
+        /*double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+        //Raiz quadrada
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.println("Raiz quadrada de " + x + " = " + A);
+        System.out.println("Raiz quadrada de " + y + " = " + B);
+        System.out.println("Raiz quadrada de 25 = " + C);
+
+        //Valor elevado
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+
+        //Valor absoluto
+        A = Math.abs(y);
+        B = Math.abs(z);
+
+        System.out.println("Valor absoluto de " + y + " = " + A);
+        System.out.println("Valor absoluto de " + z + " = " + B);*/
+
+
+
+        // && = E: Todas as condições devem ser vedadeiras,
+        // || = OU: Pelo menos uma das condições deve ser vedadeira,
+        // ! = NÃO: operador inverte a condição
+
+        int x = 5;
+
+        System.out.println("Bom dia");
+
+        if (x > 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+
+       /* Scanner sc = new Scanner(System.in);
+        int minutos = sc.nextInt();
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
+        }
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+        sc.close();
+*/
+
+
+
+
+    }
+}
